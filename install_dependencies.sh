0184f8c508f987540dbf149de80407b2c3267f73
Brandon Ogle <brandon@standardcognition.com>
2024-10-14 23:25:05 -0700

    Remove install deps script, just use uv

--- install_dependencies.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# install_dependencies.sh
-
-# Exit immediately if a command exits with a non-zero status
-set -e
-
-uv pip install -r <(uv pip compile pyproject.toml --extra dev)
